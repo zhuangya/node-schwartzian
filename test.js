849a474fa8d80dd8be117a21f39e3dcd1b546cfa
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-04-12 21:08:25 +0800

    test: ensure default and named export

--- test.js
+++ test.js
@@ -1,17 +1,21 @@
 "use strict";
 
-import { schwartzian } from ".";
+import defaultExport, { schwartzian as namedExport } from ".";
 
-test("should sort numbers by default", () => {
-  const num = [2, 1, 3, 9, 5];
-  expect(schwartzian(num)).toEqual([1, 2, 3, 5, 9]);
-});
+[defaultExport, namedExport].forEach((schwartzian) => {
+  describe("named export", () => {
+    test("should sort numbers by default", () => {
+      const num = [2, 1, 3, 9, 5];
+      expect(schwartzian(num)).toEqual([1, 2, 3, 5, 9]);
+    });
 
-test("should sort object with custom mapper", () => {
-  const obj = [{ weight: 38 }, { weight: 9 }, { weight: 10 }];
-  expect(schwartzian(obj, (x) => x.weight)).toEqual([
-    { weight: 9 },
-    { weight: 10 },
-    { weight: 38 },
-  ]);
+    test("should sort object with custom mapper", () => {
+      const obj = [{ weight: 38 }, { weight: 9 }, { weight: 10 }];
+      expect(schwartzian(obj, (x) => x.weight)).toEqual([
+        { weight: 9 },
+        { weight: 10 },
+        { weight: 38 },
+      ]);
+    });
+  });
 });
