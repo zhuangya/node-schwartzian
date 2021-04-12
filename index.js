67e9d49adb2dd04814c8208bd6942480d6d4b869
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-04-12 21:03:18 +0800

    chore: enable esm support

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-'use strict';
+"use strict";
 
-const defaultMapper = x => x;
+const defaultMapper = (x) => x;
 const defaultComparer = (a, b) => a - b;
 
-module.exports = (array, mapper=defaultMapper, comparer=defaultComparer) =>
-  array.map((item, index) => ({ index, value: mapper(item) }))
-      .sort((a, b) => comparer(a.value, b.value))
-      .map(({ index }) => array[index]);
+export function schwartzian(
+  array,
+  mapper = defaultMapper,
+  comparer = defaultComparer
+) {
+  return array
+    .map((item, index) => ({ index, value: mapper(item) }))
+    .sort((a, b) => comparer(a.value, b.value))
+    .map(({ index }) => array[index]);
+}
+
+export default schwartzian;
--- test.js
+++ test.js
@@ -1,14 +1,17 @@
-'use strict';
+"use strict";
 
-const schwartzian = require('./');
+import { schwartzian } from ".";
 
-
-test('should sort numbers by default', () => {
+test("should sort numbers by default", () => {
   const num = [2, 1, 3, 9, 5];
   expect(schwartzian(num)).toEqual([1, 2, 3, 5, 9]);
 });
 
-test('should sort object with custom mapper', () => {
+test("should sort object with custom mapper", () => {
   const obj = [{ weight: 38 }, { weight: 9 }, { weight: 10 }];
-  expect(schwartzian(obj, x => x.weight)).toEqual([{ weight: 9 }, { weight: 10 }, { weight: 38 }]);
+  expect(schwartzian(obj, (x) => x.weight)).toEqual([
+    { weight: 9 },
+    { weight: 10 },
+    { weight: 38 },
+  ]);
 });
